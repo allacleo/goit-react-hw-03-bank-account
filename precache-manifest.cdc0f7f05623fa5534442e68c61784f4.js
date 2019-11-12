6c2d6f6f83c2b95fbfac7b62a51c36271397aa8f
Alla Sariieva <alla.cleo@gmail.com>
2019-11-12 15:12:12 +0200

    Updates

---
+++ precache-manifest.cdc0f7f05623fa5534442e68c61784f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e11029f1223e1c8105ee555fbb78f75",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "106309c3fa8a7e4cb591",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "fcfe7a339283a76b6e6d",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.a22160cb.chunk.css"
+  },
+  {
+    "revision": "106309c3fa8a7e4cb591",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.8b5484e7.chunk.js"
+  },
+  {
+    "revision": "fcfe7a339283a76b6e6d",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.ab02f0cb.chunk.js"
+  },
+  {
+    "revision": "39c8f443f735b654887f",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.8a6034d8.js"
+  }
+]);
